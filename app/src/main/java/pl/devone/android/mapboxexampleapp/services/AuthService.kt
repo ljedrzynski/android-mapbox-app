900b863c398065efd5236417ac3686a2ccc70616
ljedrzynski <lukjedrzynski@gmail.com>
2018-01-18 21:38:39 +0100

    init

---
+++ app/src/main/java/pl/devone/android/mapboxexampleapp/services/AuthService.kt
@@ -0,0 +1,11 @@
+package pl.devone.android.mapboxexampleapp.services
+
+/**
+ * Created by ljedrzynski on 14.01.2018.
+ */
+class AuthService {
+
+   fun get() : String {
+        return  "";
+    }
+}
